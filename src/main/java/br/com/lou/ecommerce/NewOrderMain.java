7e87c3ada0f0535625fd91ce360b12ed1c1d4881
pc asus <jrsilva.lorivas@gmail.com>
2025-10-12 20:27:08 -0300

    Criando produtores em java

--- src/main/java/br/com/lou/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/lou/ecommerce/NewOrderMain.java
@@ -29,7 +29,6 @@ public class NewOrderMain {
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-//        properties.setProperty(ProducerConfig.MAX_BLOCK_MS_CONFIG, "120000"); // 120 segundos
         return properties;
     }
 }
